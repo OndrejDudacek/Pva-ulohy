eb939e856b39310c8ad0ac4874395345cf207464
OndrejDudacek <ondrej.dudacek@gmail.com>
2024-04-02 10:33:01 +0200

    Refactor code in 4/4.py and remove unnecessary test files

--- 4/4.py
+++ 4/4.py
@@ -1,7 +1,5 @@
+import sys
 
-for i in range(6):
-    with open(f"./tets/000{i}_in.txt", "r") as file:
-        content = file.read()
-        print(content)
-        print(i)
-        print("")
+print(f"in:\n{sys.argv[1]}")
+print(f"out:\n{sys.argv[2]}")
+print("-----------------------------------")
---
+++ 4/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in {0000..0005}; do
+    input_file="./000${i}_in.txt"
+    output_file="./000${i}_out.txt"
+    
+    if [ -e "$input_file" ]; then
+        input=$(cat $input_file)
+        output=$(cat $output_file)
+
+        python3 4.py "$input" "$output"
+    else
+        echo "Input file $input_file not found."
+    fi
+done
