109327c0a0f796b3e902c1d129599105710e4798
OndrejDudacek <ondrej.dudacek@gmail.com>
2024-04-03 11:08:10 +0200

    Refactor input validation in 4/4.py

--- 4/4.py
+++ 4/4.py
@@ -1,5 +1,8 @@
 import sys
 
-print(f"in:\n{sys.argv[1]}")
-print(f"out:\n{sys.argv[2]}")
-print("-----------------------------------")
+numSequence = sys.argv[1].split()
+
+if (len(numSequence) == 0 or len(numSequence) > 2000 or not all(map(lambda x: x.isdigit(), numSequence))):
+    print("Nespravny vstup")
+    sys.exit()
+
