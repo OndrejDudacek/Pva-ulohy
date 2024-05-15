aec1040d425a6545d29a3ac18ba6e4cda6bb9c45
OndrejDudacek <ondrej.dudacek@gmail.com>
2024-05-15 23:28:15 +0200

    almost done 5, without wrong inpout handling

--- 5/5.js
+++ 5/5.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const fs = require("fs");
 
 const inputs = [];
@@ -12,7 +13,6 @@ const findInputsAndOutputs = () => {
       outputs.push(file);
     }
   }
-  console.log(inputs, outputs);
 };
 
 const readInputAndOutput = (input, output) => {
@@ -21,17 +21,64 @@ const readInputAndOutput = (input, output) => {
   return [inputFileContents, outputFileContents];
 };
 
-const checkIfCorrect = (output, expectedOutput) => {
-  if (output === expectedOutput) {
-    console.log(output, "X", expectedOutput, "✅\n________________________\n");
-  } else {
-    console.log(output, "X", expectedOutput, "❌\n________________________\n");
-  }
-};
-
 const generateOutput = (message) => {
   return `Posloupnost:\n${message}`;
 };
 
+const calculateDistances = (planes) => {
+  let results = {};
+  for (let first = 0; first < planes.length; first++) {
+    for (let second = first + 1; second < planes.length; second++) {
+      const distance =
+        Math.round(
+          Math.sqrt(
+            (planes[first][0][0] - planes[second][0][0]) ** 2 +
+              (planes[first][0][1] - planes[second][0][1]) ** 2
+          ) * 1000000
+        ) / 1000000;
+      //console.log(distance);
+      results[`${planes[first][1]} - ${planes[second][1]}`] = distance;
+    }
+  }
+  return results;
+};
+
 findInputsAndOutputs();
-console.log(readInputAndOutput(inputs[0], outputs[0]));
+for (const inputIndex in inputs) {
+  const [input, expectedOutput] = readInputAndOutput(
+    inputs[inputIndex],
+    outputs[inputIndex]
+  );
+  let planes = input.split("\n");
+  for (const i in planes) {
+    planes[i] = planes[i].split(": ");
+    planes[i][0] = planes[i][0].split(",");
+    planes[i][0] = planes[i][0].map((x) => parseFloat(x));
+    if (planes[i].length !== 2) {
+      console.log(expectedOutput + "\n---");
+      console.log(`Pozice letadel:\nNesprávný vstup.`);
+      console.log("\n-------------------------------------------\n");
+    }
+  }
+  console.log(planes);
+  let distances = calculateDistances(planes);
+  let smallestDistance = Number.MAX_SAFE_INTEGER;
+  for (const distance in distances) {
+    if (distances[distance] < smallestDistance) {
+      smallestDistance = distances[distance];
+    }
+  }
+  for (const planes in distances) {
+    if (distances[planes] !== smallestDistance) {
+      delete distances[planes];
+    }
+  }
+  console.log(expectedOutput + "\n---");
+  console.log(`Pozice letadel:
+Vzdalenost nejblizsich letadel: ${smallestDistance}
+Nalezenych dvojic: ${Object.keys(distances).length}`);
+  for (const keys in distances) {
+    console.log(keys);
+  }
+  console.log("\n-------------------------------------------\n");
+}
